7401aff39b7262c37f1c7bf94fd3c826c136767b
shuij <864805002@qq.com>
2025-08-20 15:03:44 +0800

    Revert "暂存"
    This reverts commit e1097b352e2b96d0c803dd137fcd08c7df28dfb2.

--- hamapp/src/main/java/com/mm/hamcompose/data/db/hotkey/HotKeyDatabase.kt
+++ hamapp/src/main/java/com/mm/hamcompose/data/db/hotkey/HotKeyDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import com.mm.hamcompose.data.bean.Hotkey
 import com.mm.hamcompose.data.db.DbConst
 
-@Database(entities = [Hotkey::class], version = DbConst.dbVersion,exportSchema = false)
+@Database(entities = [Hotkey::class], version = DbConst.dbVersion)
 abstract class HotkeyDatabase: RoomDatabase() {
     abstract fun hotkeyDao(): HotkeysDao
 }
